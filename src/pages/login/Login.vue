62d922ad09f4bc41336858aab4c242a1305c5a62
JrzenonDev <jrobertoonb@gmail.com>
2021-07-27 23:36:07 -0300

    chore: create form in component login

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,7 +1,22 @@
 <template>
-  <main>
-    Login
-  </main>
+  <form class="form-login">
+    <div class="card">
+      <div class="card-header text-center">
+        <h1>Expanses</h1>
+      </div>
+      <div class="card-body">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="E-mail">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Senha">
+        </div>
+        <button class="btn btn-primary w-100">
+          Enviar
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -10,3 +25,17 @@ export default {
   name: 'Login'
 }
 </script>
+
+<style lang="scss" scoped>
+  .form-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    .card {
+      width: 25%;
+      color: var(--darker);
+    }
+  }
+</style>
