6f3cbb4b0ea2602bbcd7e7f000f69efbfceaf066
JrzenonDev <jrobertoonb@gmail.com>
2021-08-20 00:45:45 -0300

    navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="app">
     <base-spinner/>
-    <router-view/>
+    <div class="contaier-fluid" v-if="isLogged">
+      <div class="row">
+        <div class="col-lg-3">
+          <layout-navigation />
+        </div>
+        <div class="col">
+          <router-view/>
+        </div>
+      </div>
+    </div>
+    <router-view v-else />
   </div>
 </template>
 
 <script>
 import BaseSpinner from './components/global/BaseSpinner'
+import LayoutNavigation from './components/layout/LayoutNavigation.vue'
 
 export default {
   name: 'App',
   components: {
-    BaseSpinner
+    BaseSpinner,
+    LayoutNavigation
+  },
+  data () {
+    return {
+      isLogged: false
+    }
   },
   mounted () {
     this.$firebase.auth().onAuthStateChanged(user => {
       window.uid = user ? user.uid : null
+      this.isLogged = !!user
       if (window.uid) {
         if (this.$router.currentRoute.name !== 'home') {
           this.$router.push({ name: 'home' })
