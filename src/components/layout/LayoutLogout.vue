2deef992955df58efea63bdc8a8ae342028cf5ff
JrzenonDev <jrobertoonb@gmail.com>
2021-08-24 22:37:43 -0300

    create layout component Logout

---
+++ src/components/layout/LayoutLogout.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-logout">
+    <i class="fa fa-power-off"></i>
+    Logout
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'LayoutLogout'
+}
+</script>
+
+<style lang="scss" scoped>
+  .btn-logout {
+    width: 100%;
+    cursor: pointer;
+    color: var(--white);
+    &:hover {
+      color: var(--featured);
+    }
+  }
+</style>
--- src/components/layout/LayoutNavigation.vue
+++ src/components/layout/LayoutNavigation.vue
@@ -1,23 +1,31 @@
 <template>
-  <ul class="nav flex-column">
-    <router-link
-      v-for="(router, i) in routerLink"
-      :key="i"
-      class="nav-link"
-      :to="{name: router.name}"
-      exact
-      exact-active-class="active"
-    >
-      <i :class="`fa fa-${router.meta.icon}`" />
-      {{ router.meta.title }}
-    </router-link>
+  <ul class="nav flex-column justify-content-between">
+    <div>
+      <router-link
+        v-for="(router, i) in routerLink"
+        :key="i"
+        class="nav-link"
+        :to="{name: router.name}"
+        exact
+        exact-active-class="active"
+      >
+        <i :class="`fa fa-${router.meta.icon}`" />
+        {{ router.meta.title }}
+      </router-link>
+    </div>
+
+    <layout-logout />
   </ul>
 </template>
 
 <script>
+import LayoutLogout from './LayoutLogout.vue'
 
 export default {
   name: 'LayoutNavigation',
+  components: {
+    LayoutLogout
+  },
   computed: {
     routerLink () {
       return this.$router.options.routes.filter(r => r.name !== 'login')
