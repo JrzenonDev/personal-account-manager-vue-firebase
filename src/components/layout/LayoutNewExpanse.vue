5ad8fdbdeff71aac766639df5366b66d3fa1a3b2
JrzenonDev <jrobertoonb@gmail.com>
2021-08-25 00:06:46 -0300

    create inputs and ini v-model in component new expanse

--- src/components/layout/LayoutNewExpanse.vue
+++ src/components/layout/LayoutNewExpanse.vue
@@ -7,39 +7,64 @@
       <i class="fa fa-plus"></i>
       Novo gasto
     </button>
-    <div
-      :class="{show: showModal}"
-      class="modal fade"
-      :style="{display: showModal ? 'block' : 'none'}"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLiveLabel">Modal title</h5>
-            <button
-              @click="closeModal()"
-              type="button"
-              class="close"
-            >
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Woohoo, you're reading this text in a modal!</p>
-          </div>
-          <div class="modal-footer">
-            <button
-              @click="closeModal()"
-              type="button"
-              class="btn btn-secondary"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+    <form @submit.prevent="submit()">
+      <div
+        :class="{show: showModal}"
+        class="modal fade"
+        :style="{display: showModal ? 'block' : 'none'}"
+      >
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLiveLabel">Adicionar um novo gasto</h5>
+              <button
+                @click="closeModal()"
+                type="button"
+                class="close"
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <div class="form-group col-8">
+                  <label for="descricao">Descrição:</label>
+                  <input
+                    v-model="form.description"
+                    type="text"
+                    class="form-control"
+                    required
+                  >
+                </div>
+                <div class="form-group col-4">
+                <label for="valor">Valor (R$):</label>
+                  <input
+                    v-model="form.value"
+                    type="text"
+                    class="form-control"
+                    required
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                @click="closeModal()"
+                type="button"
+                class="btn btn-secondary"
+              >
+                Fechar
+              </button>
+              <button
+                class="btn btn-primary"
+              >
+                Incluir novo gasto
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
     <div
       :class="{show: showModal}"
       :style="{display: showModal ? 'block' : 'none'}"
@@ -52,11 +77,26 @@
 
 export default {
   name: 'LayoutNewExpanse',
-  data: () => ({ showModal: false }),
+  data: () => ({
+    showModal: false,
+    form: {
+      value: '',
+      description: ''
+    }
+  }),
   methods: {
+    submit () {
+      console.log('ok')
+    },
     closeModal () {
       this.showModal = false
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .modal {
+    color: var(--darker)
+  }
+</style>
