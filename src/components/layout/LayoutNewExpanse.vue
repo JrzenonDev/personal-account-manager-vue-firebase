c59a302b87dfc96a3e6a4a5fdcae9c52ecc62eb2
JrzenonDev <jrobertoonb@gmail.com>
2021-08-24 23:33:05 -0300

    method close and open modal

--- src/components/layout/LayoutNewExpanse.vue
+++ src/components/layout/LayoutNewExpanse.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="w-100 d-flex justify-content-center">
-    <button class="btn btn-lg btn-outline-primary w-75">
+    <button
+      @click="showModal = true"
+      class="btn btn-lg btn-outline-primary w-75"
+    >
       <i class="fa fa-plus"></i>
       Novo gasto
     </button>
-    <div class="modal fade show" style="display: block;">
+    <div
+      :class="{show: showModal}"
+      class="modal fade"
+      :style="{display: showModal ? 'block' : 'none'}"
+    >
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLiveLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <button
+              @click="closeModal()"
+              type="button"
+              class="close"
+            >
               <span aria-hidden="true">×</span>
             </button>
           </div>
@@ -17,19 +28,35 @@
             <p>Woohoo, you're reading this text in a modal!</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary">Close</button>
+            <button
+              @click="closeModal()"
+              type="button"
+              class="btn btn-secondary"
+            >
+              Close
+            </button>
             <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
     </div>
-    <div class="modal-backdrop fade show"></div>
+    <div
+      :class="{show: showModal}"
+      :style="{display: showModal ? 'block' : 'none'}"
+      class="modal-backdrop fade">
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'LayoutNewExpanse'
+  name: 'LayoutNewExpanse',
+  data: () => ({ showModal: false }),
+  methods: {
+    closeModal () {
+      this.showModal = false
+    }
+  }
 }
 </script>
