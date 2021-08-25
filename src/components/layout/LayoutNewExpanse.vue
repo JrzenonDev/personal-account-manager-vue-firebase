c7fbcb3347111e6047d59fd923769d27d416003d
JrzenonDev <jrobertoonb@gmail.com>
2021-08-24 23:16:45 -0300

    create modal in component new expanse

--- src/components/layout/LayoutNewExpanse.vue
+++ src/components/layout/LayoutNewExpanse.vue
@@ -4,6 +4,26 @@
       <i class="fa fa-plus"></i>
       Novo gasto
     </button>
+    <div class="modal fade show" style="display: block;">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLiveLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Woohoo, you're reading this text in a modal!</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop fade show"></div>
   </div>
 </template>
 
