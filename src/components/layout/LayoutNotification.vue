c9b5c8c72dec3396b9a8f0454f8ed0d827a9b860
JrzenonDev <jrobertoonb@gmail.com>
2021-08-28 19:51:16 -0300

    create notification functionality

--- src/components/layout/LayoutNotification.vue
+++ src/components/layout/LayoutNotification.vue
@@ -1,10 +1,53 @@
 <template>
-  <h2>Notification</h2>
+  <div
+    v-if="show"
+    :class="`alert-${type}`"
+    class="alert alert-warning alert-dismissible fade show"
+  >
+    {{ message }}
+    <button
+      @click="close()"
+      type="button"
+      class="close"
+    >
+      <span>&times;</span>
+    </button>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'LayoutNotification'
+  name: 'LayoutNotification',
+  data: () => ({
+    show: false,
+    message: '',
+    type: 'danger'
+  }),
+  created () {
+    this.$root.$on('Notification::show', payload => {
+      this.show = true
+      this.type = payload.type
+      this.message = payload.message
+
+      setTimeout(() => { this.close() }, payload.timeout || 4000)
+    })
+  },
+  methods: {
+    close () {
+      this.message = ''
+      this.type = 'danger'
+      this.show = false
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .alert {
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    max-width: 400px;
+  }
+</style>
