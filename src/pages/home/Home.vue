e425b1e6cd3ecb4e92f9406f0aee6ca9aa52854a
JrzenonDev <jrobertoonb@gmail.com>
2021-08-29 01:39:43 -0300

    structured layout component home with static data

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,27 @@
 <template>
-  <main>
-    Home
-  </main>
+  <div class="home">
+    <div class="row">
+      <div class="col-6 home-box">
+        <small>Você gastou</small>
+        <div class="money">R$ 500.00</div>
+        <small>Em 89 compras</small>
+      </div>
+      <div class="col-6 home-box">
+        <small>A média de gasto é de</small>
+        <div class="money">R$ 81.81</div>
+      </div>
+      <div class="col-6 home-box">
+        <small>A maior compra foi de</small>
+        <div class="money">R$ 81.81</div>
+        <small>No dia 10/08/2021</small>
+      </div>
+      <div class="col-6 home-box">
+        <small>A menor compra foi de</small>
+        <div class="money">R$ 2.00</div>
+        <small>No dia 20/08/2021</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,3 +48,31 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    padding: 15px;
+    font-size: 30pt;
+
+    .home-box {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      height: calc(50vh - 15px);
+      border: 1px solid var(--dark-medium);
+      small {
+        font-size: 1.3rem;
+      }
+      .money {
+        color: var(--featured);
+      }
+      &:nth-child(2), &:nth-child(4) {
+        border-left: none;
+      }
+      &:nth-child(3), &:nth-child(4) {
+        border-top: none;
+      }
+    }
+  }
+</style>
