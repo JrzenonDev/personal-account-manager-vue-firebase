37cba2e11e25adf2ec5b1e247735907c8f78fd72
JrzenonDev <jrobertoonb@gmail.com>
2021-09-09 23:18:10 -0300

    create component ExpanseListItem

---
+++ src/pages/expanses-list/ExpanseListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="expanse-list-item row">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ExpanseListItem'
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    font-size: 15pt;
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+</style>
